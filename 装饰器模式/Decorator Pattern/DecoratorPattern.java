5742ee4a6d31b4b11f06f1deed3f0a6ded1a0e5c
114404517@qq.com <114404517@qq.com>
2023-09-08 23:44:17 +0800

    装饰器模式

--- 装饰器模式/Decorator Pattern/DecoratorPattern.java
+++ 装饰器模式/Decorator Pattern/DecoratorPattern.java
@@ -13,12 +13,45 @@ interface Enemy
     void Attack();
 }
 
-//可可利亚的具体类，这个存放的是正常模式下的可可利亚的属性
+//可可利亚的具体类，这个存放的是正常模式下的可可利亚的属性，可以进行普通攻击
 class Kekeliya implements Enemy
 {
 
     @Override
     public void Attack() {
         System.out.println("冰寒的审判！");
+    }
+}
+
+//在模拟宇宙下，可可利亚除了原有的能力之外，又获得了新的召唤冰风的能力
+//使用装饰类进行修饰
+class KekeliyaDecorator
+{
+    //存放原来可可利亚的对象
+    public Kekeliya kekeliya;
+    public KekeliyaDecorator(Kekeliya kekeliya)
+    {
+        this.kekeliya = kekeliya;
+    }
+
+    //覆盖原有的方法（如果原有类是一个接口那么就重写）
+    public void Attack()
+    {
+        kekeliya.Attack();
+    }
+
+    //增添新的方法，实现修饰器模式的功能
+    public void SpecialBehaviour()
+    {
+        System.out.println("释放冰风！");
+    }
+}
+
+//测试代码
+public class DecoratorPattern
+{
+    public static void main(String[] args) {
+        new KekeliyaDecorator(new Kekeliya()).Attack();
+        new KekeliyaDecorator(new Kekeliya()).SpecialBehaviour();
     }
 }
